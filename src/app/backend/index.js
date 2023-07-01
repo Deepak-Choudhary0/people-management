fbce0f06f11d9fb987ebd35a8732b735732127aa
Deepak Choudhary <deepakchoudharysl78@gmail.com>
2023-07-01 14:49:41 +0530

    updated the backend file

--- src/app/backend/index.js
+++ src/app/backend/index.js
@@ -4,130 +4,125 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors()); //for cross origin resource sharing
+app.use(express.json()); //For parsing data into json
+
 
 const port = 3000;
-
 const mongoURI = 'mongodb://127.0.0.1:27017/person';
 
 const connectToMongoDB = async () => {
   try {
+    //connect to mongodb
     const client = await MongoClient.connect(mongoURI);
-    const db =client.db('person');
-    console.log('Connected to MongoDB database Person.');
 
-    //try
+    //connect to specific database
+    const db =client.db('person');
+    console.log("Connected to MongoDB database 'Person'.");
+
+    //connect to specific collection in mongodb
     const collectionName = 'person_data';
     const collectionExists = await db.listCollections({ name: collectionName }).hasNext();
 
     if (collectionExists) {
       console.log(`The collection '${collectionName}' exists in the 'person' database.`);
       
-      const collection = db.collection('person_data');
-    
-    // CRUD on MongoDB
-
-    // // Create data
-
-    //   // Single document insertion
-    //   const document = { name: 'John Doe', age: 30 };
-    //   const result = await collection.insertOne(document);
-    //   console.log('Inserted document ID:', result.insertedId);
-
-    //   // Multiple document insertion
-    //   var documents = [
-    //     { name: 'Jane Smith', age: 25 },
-    //     { name: 'Mike Johnson', age: 35 },
-    //   ];
-    //   const results = await collection.insertMany(documents);
-    //   console.log('Inserted document IDs:', results.insertedIds);
-
-    //Reading Part
+      const collection = db.collection(collectionName);
       var documents = await collection.find().toArray();
-      // console.log('\nAll documents:',documents.length,'\n');
+      // CRUD on MongoDB
 
-      // //Get keys for index 0, as other keys gonna be same too for other indixes
-      // const keys = Object.keys(documents[0]);
-      // // Iterate through the object properties
-      // for (let i=0;i<documents.length;i++){
-      //   console.log(`Data ID: ${i}`);
-      //   for (x in keys){
-      //     key=keys[x];
-      //     console.log(`==> ${key} : ${documents[i][key]}`);
-      //   }
-      // };
+      // // Create data
 
-    // Update
-      // const documentToUpdate = { _id: documents[0][keys[0]] };
-      // const updateFields = { $set: { age: 40 } };
-      // const updateResult = await collection.updateOne(documentToUpdate, updateFields);
-      // console.log('\nUpdated document count:', updateResult.modifiedCount,'\n');
+      //   // Single document insertion
+      //   const document = { name: 'John Doe', age: 30 };
+      //   const result = await collection.insertOne(document);
+      //   console.log('Inserted document ID:', result.insertedId);
 
-      // console.log('Updated Data:');
+      //   // Multiple document insertion
+      //   var documentMany = [
+      //     { name: 'Jane Smith', age: 25 },
+      //     { name: 'Mike Johnson', age: 35 },
+      //   ];
+      //   const results = await collection.insertMany(documentMany);
+      //   console.log('Inserted document IDs:', results.insertedIds);
 
-      // //Get data again from DB
-      // documents = await collection.find().toArray();
-      // for (x in keys){
-      //   key=keys[x];
-      //   console.log(`==> ${key} : ${documents[0][key]}`);
-      // }
+      // // Reading Part
+        // console.log('\nAll documents:',documents.length,'\n');
 
-    // // Delete
-    //   const documentToDelete = { _id: documents[0][keys[0]] }; // Replace <documentId> with the actual document ID
-    //   const deleteResult = await collection.deleteOne(documentToDelete);
-    //   console.log('\nDeleted document count:', deleteResult.deletedCount,'\n');
+        // //Get keys for index 0, as other keys gonna be same too for other indixes
+        // const keys = Object.keys(documents[0]);
+        // // Iterate through the object properties
+        // for (let i=0;i<documents.length;i++){
+        //   console.log(`Data ID: ${i}`);
+        //   for (x in keys){
+        //     key=keys[x];
+        //     console.log(`==> ${key} : ${documents[i][key]}`);
+        //   }
+        // };
 
-    //   console.log('Data after deletion:');
+      // Update
+        // const documentToUpdate = { _id: documents[0][keys[0]] };
+        // const updateFields = { $set: { age: 40 } };
+        // const updateResult = await collection.updateOne(documentToUpdate, updateFields);
+        // console.log('\nUpdated document count:', updateResult.modifiedCount,'\n');
 
-    //   //Get data again from DB
-    //   documents = await collection.find().toArray();
-    //   for (let i=0;i<documents.length;i++){
-    //     console.log(`Data ID: ${i}`);
-    //     for (x in keys){
-    //       key=keys[x];
-    //       console.log(`==> ${key} : ${documents[i][key]}`);
-    //     }
-    //   };
+        // console.log('Updated Data:');
 
-    //Query
-      // const query = { age: { $gte: 35 } }; // Example query to find documents with age greater than or equal to 30
-      // const documentsMatching = await collection.find(query).toArray();
-      // if (documentsMatching.length > 0) {
-      //   console.log('\nMatching documents:',documentsMatching.length,'\n');
-      //   for (let i=0;i<documentsMatching.length;i++){
-      //     console.log(`Matching ID: ${i}`);
+        // //Get data again from DB
+        // documents = await collection.find().toArray();
+        // for (x in keys){
+        //   key=keys[x];
+        //   console.log(`==> ${key} : ${documents[0][key]}`);
+        // }
+
+      // // Delete
+      //   const documentToDelete = { _id: documents[0][keys[0]] }; // Replace <documentId> with the actual document ID
+      //   const deleteResult = await collection.deleteOne(documentToDelete);
+      //   console.log('\nDeleted document count:', deleteResult.deletedCount,'\n');
+
+      //   console.log('Data after deletion:');
+
+      //   //Get data again from DB
+      //   documents = await collection.find().toArray();
+      //   for (let i=0;i<documents.length;i++){
+      //     console.log(`Data ID: ${i}`);
       //     for (x in keys){
       //       key=keys[x];
-      //       console.log(`==> ${key} : ${documentsMatching[i][key]}`);
+      //       console.log(`==> ${key} : ${documents[i][key]}`);
       //     }
       //   };
-      // } else {
-      //   console.log('No matching documents found.');
-      // }
+
+      //Query
+        // const query = { age: { $gte: 35 } }; // Example query to find documents with age greater than or equal to 30
+        // const documentsMatching = await collection.find(query).toArray();
+        // if (documentsMatching.length > 0) {
+        //   console.log('\nMatching documents:',documentsMatching.length,'\n');
+        //   for (let i=0;i<documentsMatching.length;i++){
+        //     console.log(`Matching ID: ${i}`);
+        //     for (x in keys){
+        //       key=keys[x];
+        //       console.log(`==> ${key} : ${documentsMatching[i][key]}`);
+        //     }
+        //   };
+        // } else {
+        //   console.log('No matching documents found.');
+        // }
     } 
     else {
       console.log(`The collection '${collectionName}' does not exist in the 'person' database.`);
     }
 
-    // Middleware to parse JSON bodies
-    app.use(express.json());
-
-    // Sample data
-    // let users = [
-    //   { id: 1, name: 'John Doe', age: 17 },
-    //   { id: 2, name: 'Jane Smith', age: 26 }
-    // ];
-
-    const collection = db.collection('person_data');
-    var documents = await collection.find().toArray();
-
     // Route handler for GET /person
-    app.get('/person', (req, res) => {
+    app.get('/person', async (req, res) => {
+      const collection = db.collection(collectionName);
+      var documents = await collection.find().toArray();
       res.json(documents);
     });
 
     // Route handler for POST /person
-    app.post('/person', async(req, res)  => {
+    app.post('/person', async (req, res)  => {
+      const collection = db.collection(collectionName);
+      var documents = await collection.find().toArray();
       var id = documents.length + 1;
       const { name, age, gender, mob } = req.body; // Extracting data from the request body
     
@@ -135,14 +130,16 @@ const connectToMongoDB = async () => {
     
       const result = await collection.insertOne(newPerson);
       console.log('Inserted document ID:', result.insertedId);
-      documents.push(newPerson); // Adding the new person to the users array
+      // documents.push(newPerson); // Adding the new person to the users array
       res.json('Success.');
       // res.redirect('/person'); // Redirecting to the '/person' route
     });
 
 
-    // GET /person/:id - Display a form to edit a person with a specified ID
-    app.get('/person/:id', (req, res) => {
+    // GET /person/:id - Display details of specified ID
+    app.get('/person/:id', async (req, res) => {
+      const collection = db.collection(collectionName);
+      var documents = await collection.find().toArray();
       const id = parseInt(req.params.id);
       const person = documents.find(p => p.id === id);
       if (!person) {
@@ -153,7 +150,9 @@ const connectToMongoDB = async () => {
     });
 
     // PUT /person/:id - Update a person with a specified ID
-    app.put('/person/:id', (req, res) => {
+    app.put('/person/:id', async (req, res) => {
+      const collection = db.collection(collectionName);
+      var documents = await collection.find().toArray();
       const id = parseInt(req.params.id);
       const { name, age, gender, mob } = req.body;
       const personIndex = documents.findIndex(p => p.id === id);
@@ -168,11 +167,13 @@ const connectToMongoDB = async () => {
 
     // DELETE /person/:id
     app.delete('/person/:id', async (req, res) => {
+      const collection = db.collection(collectionName);
+      var documents = await collection.find().toArray();
       const id = parseInt(req.params.id);
-      
+      // const person = documents.find(p => p.id === id);
       try {
         // Find and delete the person
-        const deletedPerson = await Person.findByIdAndDelete(id);
+        const deletedPerson = await documents.findByIdAndDelete(id);
     
         if (!deletedPerson) {
           res.status(404).json({ error: 'Person not found' });
@@ -196,25 +197,6 @@ const connectToMongoDB = async () => {
     app.get('/', (req, res) => {
       res.send('Welcome to the API');
       console.log('GET Empty request....');
-    });
-
-    // POST /users - Create a new user
-    app.post('/users', (req, res) => {
-      console.log('POST User request....');
-
-      const name='Deepak';
-      const email = 'deepak@gmail.com';
-      // Generate a unique ID for the new user
-      const id = users.length + 1;
-
-      // Create the new user object
-      const newUser = { id, name, email };
-
-      // Add the new user to the list
-      users.push(newUser);
-      console.log('User created....');
-
-      res.status(201).json(newUser);
     });
 
     // Start the server
